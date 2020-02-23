cef934ec3ad26b4bac87be7e2144a86e9299aaa7
navaNoobCoder <nkindelpitiya@gmail.com>
2020-02-23 14:57:30 +0530

    commit

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use('/users', usrRoutes);
 app.use('/issued_books', issuedBookRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(client/build));
+    app.use(express.static('client/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
